9748b3a33883e927bb2751dfa659fd58603bb355
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-13 08:55:16 -0700

    git

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char test_istr[] = "Programming is like building a multilingual puzzle";
+	char test_str[] = "Programming is like building a multilingual puzzle";
 
 
 	puts(test_str);
