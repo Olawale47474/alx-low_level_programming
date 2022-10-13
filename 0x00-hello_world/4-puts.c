a817761162016d240d54d72ca363702a4242d856
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-13 08:52:50 -0700

    personal

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-	char test_str[] = "\"Programming is like building a multilingual puzzle";
+	char test_istr[] = "Programming is like building a multilingual puzzle";
 
 
-	puts(str);
+	puts(test_str);
 	return (0);
 }	
 		
