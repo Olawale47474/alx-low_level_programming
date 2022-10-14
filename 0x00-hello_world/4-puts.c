f9f7c1dd3d891217e4a371efc7f13d763692a3d7
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-13 17:04:54 -0700

    files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry
+ *
  * Return: Always 0
  */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	char test_strr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(test_str);
 	return (0);
 }
