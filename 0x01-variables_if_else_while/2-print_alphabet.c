c5c78212862806839ec106962dc8b66e9710c753
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-15 05:57:12 -0700

    files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/** main: Printout lower case alphabet
+ *
+ * make sure it correct and printout 0 
+ */
+int main(void)
+{
+	char mycharacter = "g";
+	char stringcharcters[] = "my name john";
+	
+	putchar("mycharacter");
+	putchat("stringcharacters");
+
+	return 0;
+}
+
