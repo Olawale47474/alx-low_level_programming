dda60be8efb8676fd3c3df698d314be5f856b23a
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-15 08:15:08 -0700

    files

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lowercase in new line, except q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit number starting from 0 to 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 10
+		printf("my integer value = %d", num);
+	getch();
+	return (0);
+}
