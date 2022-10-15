8de46119e6b55b5ec3121ecc10de54d0db1a6deb
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-15 06:22:26 -0700

    git

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
-#include <stdlib.h>
 #include <stdio.h>
-/** main: Printout lower case alphabet
+
+/**
+ * main - Printout lower case alphabet
  *
- * make sure it correct and printout 0 
+ * Return: Correctly
  */
 int main(void)
 {
 	char mycharacter = "g";
 	char stringcharcters[] = "my name john";
-	
+
 	putchar("mycharacter");
 	putchat("stringcharacters");
-
-	return 0;
+	return (0);
 }
-
