13d7471a81647c4f02f2d7021567a701b07f2a60
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-18 09:29:40 -0700

    filled

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * main - Prints _putchar
@@ -7,10 +7,16 @@
  */
 int main(void)
 {
-	char ch = 'putchar';
-
-	putchar('_')
-	putchar(ch);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
 	return (0);
 }
