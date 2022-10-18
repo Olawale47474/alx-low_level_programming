9a83a22b316f311e3378846fdf7fb4a52760f52c
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-18 06:42:56 -0700

    filled

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char y = "putcha";
+
+	while (y)
+	{
+		_putcha(y)
+		y++;
+	}
+	_putchar('\'n);
+	return (0);
+}
+
+
+
