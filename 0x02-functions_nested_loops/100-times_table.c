c1aa7956012c714792f7b53482edfcd6093d26b7
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-19 16:13:22 -0700

    fill

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,18 @@
 #include "main.h"
-/**
- * print_times_table - Entry point
- * @n: input
- * Return: Always 0 (Success)
- */
-void print_times_table(int n)
-{
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_times_table(0);
+	_putchar('\n');
+	print_times_table(15);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);  
 	return (0);
 }
