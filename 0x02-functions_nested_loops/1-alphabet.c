bf98fcbf70037619321b599ff39c1a8c5c8e4510
Olawale47474 <Olawaleyusufola1999@gmail.com>
2022-10-19 06:47:47 -0700

    fill

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_aplphabet - prints the alpha in lowercase
+ * print_aplphabet - a function that prints the alphabet, in lowercase
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
